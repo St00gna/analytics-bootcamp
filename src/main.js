d356170a1bf2b07aa66dae3d474f76ef19d2824c
Sto0gna <borysyurevych@gmail.com>
2025-03-15 17:36:11 +0200

    install gtm

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createGtm } from '@gtm-support/vue-gtm';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(
+  createGtm({
+    id: 'G-XXXXXXXXXX', // Ваш ідентифікатор GA4
+    vueRouter: router,
+    enabled: true, // Вимикайте у dev-режимі
+    debug: true, // Виводить події у консоль
+  })
+);
+
+app.use(router);
+app.mount('#app');
